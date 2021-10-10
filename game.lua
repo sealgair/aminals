00a00816fef25e6e95349267b533582cec46a7e7
Chase Caster <sealgair@gmail.com>
2021-10-09 20:49:24 -0500

    more robust collision detection

--- game.lua
+++ game.lua
@@ -1,11 +1,6 @@
 
--- system callbacks
-
-function _init()
-end
-
 dt = 1/60
-g = 4 * dt
+g = 5 * dt
 
 flags = {
   stop=0
@@ -16,16 +11,19 @@ player = {
   flipped=false,
   x=56, y=112,
   w=8, h=8,
-  accel=.5, speed=1.5,
+  accel=4, speed=2.5,
   vx=0, vy=0,
 }
 
-function debug(val)
+function debug(val, opts)
+  opts = opts or {}
+  x = opts.x or 0
+  y = opts.y or 0
   val = tostring(val)
-  color(5)
-  rectfill(0, 121, 1+(#tostring(val)*4), 127)
-  color(6)
-  print(val, 1, 122)
+  color(opts.bg or 5)
+  rectfill(x, y, 1+(#tostring(val)*4), y+7)
+  color(opts.fg or 6)
+  print(val, x+1, y+1)
 end
 
 function wrap(min, v, max)
@@ -33,6 +31,10 @@ function wrap(min, v, max)
   if (v < min) v += d
   if (v > max) v -= d
   return v
+end
+
+function bound(b, v, t)
+  return max(b, min(v, t))
 end
 
 function sign(v)
@@ -55,28 +57,19 @@ function player:collides(x, y)
 end
 
 function player:move()
-  -- set desired new position
-  nx = self.x + self.vx
-  ny = self.y + self.vy
-
   -- collide with map tiles
-  for x=flr(self.x), nx, sign(self.vx) do
-    if self:collides(x, self.y) then
-      self.vx = 0
-      break
-    end
-    self.x = x
+  self.x += self.vx
+  sx = sign(self.vx)
+  while self:collides(self.x, self.y) do
+    self.vx = 0
+    self.x -= sx
   end
-  if (self.vx != 0) self.x = nx
-
-  for y=flr(self.y), ny, sign(self.vy) do
-    if self:collides(self.x, y) then
-      self.vy = 0
-      break
-    end
-    self.y = y
+  self.y += self.vy
+  sy = sign(self.vy)
+  while self:collides(self.x, self.y) do
+    self.vy = 0
+    self.y -= sy
   end
-  if (self.vy != 0) self.y = ny
 
   -- wrap around map
   self.x = wrap(0, self.x, 127)
@@ -84,21 +77,31 @@ function player:move()
 end
 
 function player:update()
-  self.vx = 0
-  if (btn(0)) self.vx = -self.speed
-  if (btn(1)) self.vx = self.speed
-  if (self.vx != 0) self.flipped = self.vx > 0
-  if self.touch then
-    self.vy = 0
+  dir = 0
+  if (btn(0)) dir = -1
+  if (btn(1)) dir = 1
+
+  if dir==0 then
+    -- speed down
+    self.vx = max(abs(self.vx) - self.accel*dt*2, 0) * sign(self.vx)
   else
-    self.vy += g
+    -- speed up
+    self.vx = bound(-self.speed, self.vx + self.accel*dir*dt, self.speed)
   end
+  if (self.vx != 0) self.flipped = self.vx > 0
+  self.vy += g
   self:move()
 end
 
 function player:draw()
   spr(self.sprite, self.x, self.y, 1, 1, self.flipped)
   -- debug(self.x ..', '..self.y)
+  -- debug((self.touchx or '') ..', '.. (self.touchy or ''), {y=8})
+end
+
+-- system callbacks
+
+function _init()
 end
 
 function _update60()
